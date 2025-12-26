1defb56db5f2b500252dd51f9f4288fb54252f9d
monis-iqbal-io <monisiqbal18@gmail.com>
2025-12-26 22:41:41 +0530

    Demonstrate basic Python data types
    This Python script demonstrates basic data types and operations, including string indexing (subscripting), string concatenation, integers, large numbers, floating-point values, and boolean values. It is designed to practice understanding how different data types work in Python.

---
+++ python_data_types.py
@@ -0,0 +1,18 @@
+#subscripting
+print("Hello"[-3])
+#string
+print("123" + " 456")
+
+#integer = whole number
+print(123 + 345)
+
+#large numbers
+print(123_345_789)
+
+#float= Decimal
+print(3.456787)
+
+
+#bolean
+print(True)
+print(False)
