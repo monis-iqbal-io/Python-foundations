83aadfb08cbcbb38f4a50c3be9eeab4d4324e60f
monis-iqbal-io <monisiqbal18@gmail.com>
2025-12-26 22:35:18 +0530

    Create band_name_generator.py
    This Python script generates a fun band name by combining user inputs.
    It takes the name of the city and a pet’s name as input and displays a possible band name, demonstrating basic input handling and string concatenation in Python.

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name Generator")
+x=input("What is the name of the city you grew in?\n")
+y=input("What's your pets's name?\n")
+print("Your band name could be " + x +" "+ y)
